4050f6759f2a28432fd1b549a1a02de7da857c99
PRZVL00 <josiahbautista00.com>
2023-03-17 14:55:32 +0800

    almost ready

--- OJT_Project_1/Task_Management/views.py
+++ OJT_Project_1/Task_Management/views.py
@@ -45,7 +45,8 @@ def client_home(request):
         context = {"task_list": task_list,
                    "total_active": total_active, "the_user": the_user}
         return render(request, 'html/Client_dashboard_home.html', context)
-    return redirect("index")
+    else:
+        return redirect("admin_home")
 
 
 def complete_task(request, task_id):
@@ -74,7 +75,8 @@ def client_pending(request):
         context = {"task_list": task_list,
                    "total_pending": total_pending, "the_user": the_user}
         return render(request, 'html/Client_dashboard_pending.html', context)
-    return redirect("index")
+    else:
+        return redirect("admin_home")
 
 
 def accept_task(request, task_id):
@@ -85,6 +87,7 @@ def accept_task(request, task_id):
     return redirect('client_pending')
 
 
+@login_required(login_url='index')
 def client_complete(request):
     first_name = request.user.first_name
     last_name = request.user.last_name
@@ -101,9 +104,11 @@ def client_complete(request):
         context = {"task_list": task_list,
                    "total_complete": total_complete, "the_user": the_user}
         return render(request, 'html/Client_dashboard_complete.html', context)
-    return redirect("index")
+    else:
+        return redirect("admin_home")
 
 
+@login_required(login_url='index')
 def admin_home(request):
     first_name = request.user.first_name
     last_name = request.user.last_name
@@ -119,16 +124,25 @@ def admin_home(request):
         context = {"task_list": task_list,
                    "total_active": total_active, "the_user": the_user}
         return render(request, 'html/Admin_dashboard_home.html', context)
-    return redirect("index")
+    else:
+        return redirect("client_home")
 
 
+@login_required(login_url='index')
 def admin_pending(request):
-    if request.method == "POST":
-        pass
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            pass
 
-    task_list = tasks.objects.filter(active_status="OFF")
-    context = {"task_list": task_list}
-    return render(request, 'html/Admin_dashboard_pending.html', context)
+        task_list = tasks.objects.filter(active_status="OFF")
+        context = {"task_list": task_list}
+        return render(request, 'html/Admin_dashboard_pending.html', context)
+    else:
+        return redirect("client_home")
 
 
 def delete_task(request, task_id):
@@ -137,6 +151,7 @@ def delete_task(request, task_id):
     return redirect('admin_pending')
 
 
+@login_required(login_url='index')
 def admin_complete(request):
     first_name = request.user.first_name
     last_name = request.user.last_name
@@ -153,156 +168,212 @@ def admin_complete(request):
         context = {"task_list": task_list,
                    "total_complete": total_complete, "the_user": the_user}
         return render(request, 'html/Admin_dashboard_complete.html', context)
-    return redirect("index")
+    else:
+        return redirect("client_home")
 
 
+@login_required(login_url='index')
 def admin_users(request):
-    return render(request, 'html/Admin_dashboard_users.html')
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        return render(request, 'html/Admin_dashboard_users.html')
+    else:
+        return redirect("client_home")
 
 
 def add_user(request):
-    if request.method == "POST":
-        fname = request.POST.get('fname')
-        lname = request.POST.get('lname')
-        idnum = request.POST.get('idnum')
-        cnum = request.POST.get('cnum')
-        email = request.POST.get('email')
-        pic = request.FILES['pic']
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            fname = request.POST.get('fname')
+            lname = request.POST.get('lname')
+            idnum = request.POST.get('idnum')
+            cnum = request.POST.get('cnum')
+            email = request.POST.get('email')
+            pic = request.FILES['pic']
 
-        decrypted_pass = lname + '-' + idnum
-        password = make_password(decrypted_pass)
-        username = idnum
+            decrypted_pass = lname + '-' + idnum
+            password = make_password(decrypted_pass)
+            username = idnum
 
-        position = "Employee"
+            position = "Employee"
 
-        new_user = app_users.objects.create(username=username, password=password, first_name=fname, last_name=lname, email=email,
-                                            id_number=idnum, contact_number=cnum, position=position, profile_pic=pic)
-        new_user.save()
-        return redirect("add_user")
+            new_user = app_users.objects.create(username=username, password=password, first_name=fname, last_name=lname, email=email,
+                                                id_number=idnum, contact_number=cnum, position=position, profile_pic=pic)
+            new_user.save()
+            return redirect("add_user")
 
-    return render(request, 'html/Add_user.html')
+        return render(request, 'html/Add_user.html')
+    else:
+        return redirect("client_home")
 
 
+@login_required(login_url='index')
 def admin_add_task(request):
-    if request.method == "POST":
-        task_name = request.POST.get('task_name')
-        employees = request.POST.get('employees')
-        categ = request.POST.get('categories')
-        subcateg = request.POST.get('subcategories')
-        stat = request.POST.get('stat')
-        the_details = request.POST.get('details')
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            task_name = request.POST.get('task_name')
+            employees = request.POST.get('employees')
+            categ = request.POST.get('categories')
+            subcateg = request.POST.get('subcategories')
+            stat = request.POST.get('stat')
+            the_details = request.POST.get('details')
 
-        published_date = date.today()
+            published_date = date.today()
 
-        new_task = tasks.objects.create(task_name=task_name, category=categ, subcategory=subcateg, details=the_details,
-                                        assigned_by="SAMPLE", assigned_to=employees, date_published=published_date,
-                                        date_completed="NONE", status=stat, active_status="OFF")
-        new_task.save()
-        return redirect("admin_add_task")
+            new_task = tasks.objects.create(task_name=task_name, category=categ, subcategory=subcateg, details=the_details,
+                                            assigned_by="SAMPLE", assigned_to=employees, date_published=published_date,
+                                            date_completed="NONE", status=stat, active_status="OFF")
+            new_task.save()
+            return redirect("admin_add_task")
 
-    employee_list = app_users.objects.filter(position="EMPLOYEE")
-    category_list = categories.objects.all()
-    subcategory_list = subcategories.objects.all()
-    details_list = detail.objects.all()
-    context = {"employee_list": employee_list, "category_list": category_list,
-               "subcategory_list": subcategory_list, "details_list": details_list}
-    return render(request, 'html/Admin_dashboard_add_task.html', context)
+        employee_list = app_users.objects.filter(position="EMPLOYEE")
+        category_list = categories.objects.all()
+        subcategory_list = subcategories.objects.all()
+        details_list = detail.objects.all()
+        context = {"employee_list": employee_list, "category_list": category_list,
+                   "subcategory_list": subcategory_list, "details_list": details_list}
+        return render(request, 'html/Admin_dashboard_add_task.html', context)
+    else:
+        return redirect("client_home")
 
 
+@login_required(login_url='index')
 def admin_edit(request):
-    return render(request, 'html/Admin_dashboard_edit.html')
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        return render(request, 'html/Admin_dashboard_edit.html')
+    else:
+        return redirect("client_home")
 
 
+@login_required(login_url='index')
 def category(request):
-    if request.method == "POST":
-        if 'form1' in request.POST:
-            ncat = request.POST.get('ncat')
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            if 'form1' in request.POST:
+                ncat = request.POST.get('ncat')
 
-            new_category = categories.objects.create(category=ncat.upper())
-            new_category.save()
-            return redirect("category")
+                new_category = categories.objects.create(category=ncat.upper())
+                new_category.save()
+                return redirect("category")
 
-        else:
-            dcat = request.POST.get('dcat')
-
-            selected_category = categories.objects.get(category=dcat)
-            selected_category.delete()
-            return redirect('category')
-
-    category_list = categories.objects.all()
-    context = {"category_list": category_list}
-    return render(request, 'html/Category.html', context)
-
-
-def subcategory(request):
-    if request.method == "POST":
-        if 'form1' in request.POST:
-            cat_add = request.POST.get('cat_add')
-            nsubcat = request.POST.get('nsubcat')
-
-            new_subcategory = subcategories.objects.create(
-                category=cat_add, subcategory=nsubcat.upper())
-            new_subcategory.save()
-            return redirect("subcategory")
-
-        else:
-            cat_del = request.POST.get('cat_del')
-            dsubcat = request.POST.get('dsubcat')
-
-            selected_subcategory = subcategories.objects.get(
-                category=cat_del, subcategory=dsubcat)
-            selected_subcategory.delete()
-            return redirect('subcategory')
-
-    category_list = categories.objects.all()
-    subcategory_list = subcategories.objects.all()
-    context = {"category_list": category_list,
-               "subcategory_list": subcategory_list}
-    return render(request, 'html/Subcategory.html', context)
-
-
-def steps(request):
-    if request.method == "POST":
-        if 'form1' in request.POST:
-            cat_to_add = request.POST.get('cat_to_add')
-            sub_to_add = request.POST.get('sub_to_add')
-            new_details = request.POST.get('new_details')
-
-            check_sub = detail.objects.filter(subcategory=sub_to_add)
-
-            if check_sub.count() >= 1:
-                print(check_sub.count())
             else:
-                new_step = detail.objects.create(
-                    category=cat_to_add, subcategory=sub_to_add, details=new_details)
-                new_step.save()
-                return redirect("steps")
+                dcat = request.POST.get('dcat')
 
-        elif 'form2' in request.POST:
-            detail_to_del = request.POST.get('sub_to_del')
+                selected_category = categories.objects.get(category=dcat)
+                selected_category.delete()
+                return redirect('category')
 
-            selected_detail = detail.objects.get(
-                subcategory=detail_to_del)
-            selected_detail.delete()
-            return redirect('steps')
+        category_list = categories.objects.all()
+        context = {"category_list": category_list}
+        return render(request, 'html/Category.html', context)
+    else:
+        return redirect("client_home")
 
-        elif 'form3' in request.POST:
-            detail_to_up = request.POST.get('sub_to_up')
-            new_details = request.POST.get('new_details')
 
-            selected_detail = detail.objects.get(
-                subcategory=detail_to_up)
-            selected_detail.details = new_details
-            selected_detail.save()
-            return redirect('steps')
+@login_required(login_url='index')
+def subcategory(request):
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            if 'form1' in request.POST:
+                cat_add = request.POST.get('cat_add')
+                nsubcat = request.POST.get('nsubcat')
 
-    category_list = categories.objects.all()
-    subcategory_list = subcategories.objects.all()
-    details_list = detail.objects.all()
-    context = {"category_list": category_list,
-               "subcategory_list": subcategory_list,
-               "details_list": details_list}
-    return render(request, 'html/Steps.html', context)
+                new_subcategory = subcategories.objects.create(
+                    category=cat_add, subcategory=nsubcat.upper())
+                new_subcategory.save()
+                return redirect("subcategory")
+
+            else:
+                cat_del = request.POST.get('cat_del')
+                dsubcat = request.POST.get('dsubcat')
+
+                selected_subcategory = subcategories.objects.get(
+                    category=cat_del, subcategory=dsubcat)
+                selected_subcategory.delete()
+                return redirect('subcategory')
+
+        category_list = categories.objects.all()
+        subcategory_list = subcategories.objects.all()
+        context = {"category_list": category_list,
+                   "subcategory_list": subcategory_list}
+        return render(request, 'html/Subcategory.html', context)
+    else:
+        return redirect("client_home")
+
+
+@login_required(login_url='index')
+def steps(request):
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    full_name = first_name + " " + last_name
+    username = request.user.username
+    if request.user.is_authenticated and request.user.position == 'Admin':
+        if request.method == "POST":
+            if 'form1' in request.POST:
+                cat_to_add = request.POST.get('cat_to_add')
+                sub_to_add = request.POST.get('sub_to_add')
+                new_details = request.POST.get('new_details')
+
+                check_sub = detail.objects.filter(subcategory=sub_to_add)
+
+                if check_sub.count() >= 1:
+                    print(check_sub.count())
+                else:
+                    new_step = detail.objects.create(
+                        category=cat_to_add, subcategory=sub_to_add, details=new_details)
+                    new_step.save()
+                    return redirect("steps")
+
+            elif 'form2' in request.POST:
+                detail_to_del = request.POST.get('sub_to_del')
+
+                selected_detail = detail.objects.get(
+                    subcategory=detail_to_del)
+                selected_detail.delete()
+                return redirect('steps')
+
+            elif 'form3' in request.POST:
+                detail_to_up = request.POST.get('sub_to_up')
+                new_details = request.POST.get('new_details')
+
+                selected_detail = detail.objects.get(
+                    subcategory=detail_to_up)
+                selected_detail.details = new_details
+                selected_detail.save()
+                return redirect('steps')
+
+        category_list = categories.objects.all()
+        subcategory_list = subcategories.objects.all()
+        details_list = detail.objects.all()
+        context = {"category_list": category_list,
+                   "subcategory_list": subcategory_list,
+                   "details_list": details_list}
+        return render(request, 'html/Steps.html', context)
+    else:
+        return redirect("client_home")
 
 
 def logoutuser(request):
